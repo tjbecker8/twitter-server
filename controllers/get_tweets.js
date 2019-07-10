d2bf60444bba314b3aa8ac59684ffd3dbaf49f5c
Tom Becker <tjbecker@me.com>
2019-07-10 11:39:11 +0700

    stopped being an idiot

--- controllers/get_tweets.js
+++ controllers/get_tweets.js
@@ -3,8 +3,8 @@ const db_tweet = require('../models/tweets')
 module.exports = (req, res) => {
 // console.log(req.query);
 	let q = {}
-	if (req.query && req.query.channel) {
-		q.channel = req.query.channel
+	if (req.query && req.query.hashtag) {
+		q.hashtag = req.query.hashtag
 	}
 	db_tweet.find(q).populate({
 		path: 'hashtag',
